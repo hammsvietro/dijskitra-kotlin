7827b456eb168f35e5779ca2970f6c2a00a27a71
Pedro Henrique Hamms VIetro <hammsvietro@gmail.com>
2020-10-23 19:34:52 -0300

    finish algorithm

--- src/main.kt
+++ src/main.kt
@@ -3,17 +3,32 @@ import java.io.InputStream
 import java.util.PriorityQueue
 
 
-const val INFINITY: Int = 9999999
+const val INFINITY: Int = 99999
 
 fun buildValuedMatrix(fileName: String): List<List<Int>> {
   val inputStream: InputStream = File(fileName).inputStream()
   val lineList = mutableListOf<String>()
   inputStream.bufferedReader().forEachLine { lineList.add(it) }
-  return lineList.map{s -> s.split(' ').map{c -> c.toInt(10)}}
+  return lineList.map{s -> s.split('\t').map{c -> c.toInt(10)}}
 }
 
-fun getVertex(vertices: List<Vertex>, desiredSource: Int): Vertex? {
-  return vertices.firstOrNull{ it.source == desiredSource }
+fun getVertex(vertices: List<Vertex>, desiredSource: Int): Vertex {
+  return vertices.first{ it.source == desiredSource }
+}
+
+fun getShortestPath(source: Int, destiny: Vertex, graph: MutableList<Vertex>): List<Int> {
+  val shortestPath: MutableList<Int> = mutableListOf()
+  var currentVertex: Vertex = destiny
+  while(true) {
+
+    shortestPath.add(currentVertex.source)
+
+    if (currentVertex.source == source) break
+
+    currentVertex = getVertex(graph, currentVertex.prev!!)
+
+  }
+  return shortestPath.reversed()
 }
 
 fun main() {
@@ -29,10 +44,10 @@ fun main() {
   println("detected ${list.size} vertex")
 
   println("type the origin vertex: ")
-  val a = readLine()?.toInt()
+  val a = readLine()!!.toInt()
 
   println("type the destination vertex: ")
-  val b = readLine()?.toInt()
+  val b = readLine()!!.toInt()
 
   for (i in list.indices) {
     val v = Vertex(i, null, if (i == a) 0 else INFINITY)
@@ -41,13 +56,13 @@ fun main() {
     graph.add(v)
   }
 
-  println(list[0].size - 1)
   while (!q.isEmpty()) {
 
     val v = q.poll()
 
+
     for(i in list[v.source].indices) {
-      if(i != v.source && i != INFINITY) {
+      if(i != v.source && list[v.source][i] != INFINITY) {
 
         val distance = list[v.source][i] + v.distance
 
@@ -62,13 +77,21 @@ fun main() {
             neighbor.prev = v.source
           }
         }
-
       }
     }
-    if(v.source == b) {
 
-      println("found shrtest path: ${v.distance}")
+    if(v.source == b) {
+      println("found shortest path with value: ${v.distance}")
     }
 
   }
+
+  val destinyVertex = getVertex(graph, b)
+
+  if(destinyVertex.distance == INFINITY) {
+    throw Error("Cannot Find Shortest Path")
+  }
+
+  println("FOUND SHORTEST PATH:\nValue: ${destinyVertex.distance}")
+  println("Path: ${getShortestPath(a, getVertex(graph, b), graph)}")
 }
